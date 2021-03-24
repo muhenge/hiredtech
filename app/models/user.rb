5d74f512d90cfe7338a3a6a16d27532da9204c81
herve <mugunga5@gmail.com>
2021-03-24 21:01:32 +0200

    Tweak devise login session

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, uniqueness: {case_sensitive: false}, format: {with: /\A[a-zA-Z0-9 _\.]*\z/}
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    condition = warden_conditions.dup
+    if login = condition.delete(:login)
+      where(condition.to_hash).where("lower(username)= :value OR lower(email) = :value", value: login.downcase).first
+    else
+      where(condition.to_hash).first
+    end
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/users/sign_out' => ' devise/sessions#destroy'
+  end
   root to: 'public#index'
   get 'public/private'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
