27cb8d1443c6cec3922c8c8c63026d9433a38b61
herve <mugunga5@gmail.com>
2021-04-03 22:40:53 +0200

    Update user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :skills
   devise_for :user do
-    get '/users/sign_out' => ' devise/sessions#destroy'
+    # get '/users/sign_out' => ' devise/sessions#destroy'
   end
 
   # devise_for :users, controllers: {
