68ebfd0f7f0c271115983fb0b9211e8ccc495010
herve <mugunga5@gmail.com>
2021-05-22 18:56:34 +0200

    Change auth root

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,6 @@ Rails.application.routes.draw do
     resources :users
   end
   devise_for :user
-    # get '/users/sign_out' => ' devise/sessions#destroy'
-
-  # devise_for :users, controllers: {
-
-  # }
-
-  # devise_scope :users do
-  #   get 'edit_user', to: 'users/registrations#edit'
-  #   # get 'signup', to: 'users/registrations#new'
-  #   # get 'signin', to: 'users/sessions#new'
-  # end
   resources :posts do
     resources :users
     resources :comments
@@ -27,25 +16,22 @@ Rails.application.routes.draw do
       put "like" => "posts#vote"
     end
   end
-
+  authenticated :user do
+    root 'posts#index', as: :authenticated_root
+  end
   resources :comments do
     resources :users
   end
-  
   resources :users, only: %i[show index] do
     resources :posts
     resources :skills
   end
-
   resources :relationships, only: [:create, :destroy] do
     resources :users
     resources :posts
   end
-
-
   get "/current_user_skills", to: "users#current_user_skills"
   root :to => "public#index"
-
   get 'public/index'
   get "/post_by_career", to: "careers#post_by_career"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
